5ff53f2b89f8090cb3a6e516ac6775e08909bf0e
Muskan Jain <86509270+JainMuskan98@users.noreply.github.com>
2023-08-10 18:00:18 +0530

    Update views.py

--- Registration/registration/app1/views.py
+++ Registration/registration/app1/views.py
@@ -49,3 +49,49 @@ def LoginPage(request):
 def LogoutPage(request):
     logout(request)
     return redirect('login')
+
+
+request. user.is_superuser:
+
+UserDepartments(department=request.POST['nDepartment'], batch=request.POST[ 'nBatch'], course=request.POST['nCourse'], classnane=request..POST['nClass'],
+createdon=datetine. now(tz=get_current_tinezone()), createdby=request.user)
+
+dept save()
+
+uploaded_file = request.FILES["document"]
+
+filepath = "uploads/" + datetime.now()._str_() + '_' + uploaded file.name
+fs = FileSystenStorage()
+
+fs.save(filepath, uploaded file)
+
+file data = open(MEDIA ROOT. _str_() + "/" + str(filepath), 'r')
+for line in file data.readlines()
+	if not 'REGISTER_NO' in line:
+		fields = line.split(",")
+		# print (fields)
+		Password = ""
+		for 1 in range(10):
+			Password += 0TPgigits[math. floor (random. randon() * 10)]
+		try:
+
+			#creating user
+
+			# auth user model columns
+
+			# password, username, First name, last name, email
+
+			user = User (usernane=fields[3]..strip(), first 					name=fields[1].strip(), lastname=rields[2].strip(),email 				Fields[3].strip(), password=make_password(Password), is_staff=False, 			is active=True)
+
+			user. save()
+
+			# user details table acts as a extension for django user model
+			# user_id (FK), reg emp_id, gender, department, batch, course, 				phoneno, role,class, password
+			userDetails = UserDetails(user_id=user, reg_emp_id=fields[6].strip(), 			gender=rields[5].strip(), password=Password, phone 					no=fields[4].strip(),role=request.POST['nuserType'].strip()
+			userDetails. save()
+		except:
+			messages. error (request,
+				“Error: The Users are alredy created / The email id is alredy 			in use
+
+messages. success(request, 'File processed successfully’)
+return redirect(’create_user-admin')
