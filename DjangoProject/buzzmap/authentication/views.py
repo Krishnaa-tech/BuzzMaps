3e10512a2798926ab7a795680c916426d0e74da5
Muskan Jain <86509270+JainMuskan98@users.noreply.github.com>
2023-09-07 11:58:37 +0530

    Create views.py

---
+++ DjangoProject/buzzmap/authentication/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home_login(request):
+    return render(request,'auth/login.html')
+
+def dashboard(request):
+    return render(request,'main/dashboard.html')
+
+def user_page(request):
+    return render(request,'main/user_page.html')
