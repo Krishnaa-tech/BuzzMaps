d54ed667cd101ad875d0a45e05f8456d1ead0b76
Krish Goyal <101107027+Krishnaa-tech@users.noreply.github.com>
2023-09-26 21:58:22 +0530

    c

--- DjangoProject/buzzmap/main/models.py
+++ DjangoProject/buzzmap/main/models.py
@@ -5,6 +5,6 @@ class users(models.Model):
     service_title=models.CharField(max_length=50)
     service_description = models.TextField(max_length=40)
 
-    table Students{
-        input 
-    }
+    # table Students{
+    #     input 
+    # }
