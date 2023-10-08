aff1b28b594ba692c6d0236c17b69ce942605beb
Krish Goyal <101107027+Krishnaa-tech@users.noreply.github.com>
2023-10-08 19:27:30 +0530

    commit

--- DjangoProject/buzzmap/buzzmap/urls.py
+++ DjangoProject/buzzmap/buzzmap/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     # path('',home_login,name='home-login'),
     path("",include('authentication.urls')),
     path("",include('main.urls')),
+    # path("",include('users.urls')),
 
 
 ]+ static(settings.STATIC_URL)
