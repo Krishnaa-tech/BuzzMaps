9a8c7f3506b16dbc9547ac6b83ca8bbeffc1912c
Krish Goyal <101107027+Krishnaa-tech@users.noreply.github.com>
2023-10-08 18:07:50 +0530

    commit

--- DjangoProject/buzzmap/authentication/urls.py
+++ DjangoProject/buzzmap/authentication/urls.py
@@ -5,7 +5,7 @@ from . import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('home/', views.home, name='home'),
 #     path('register/', views.register, name='register'),  
