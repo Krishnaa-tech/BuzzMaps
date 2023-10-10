78736f649532bec82e4224d9aa31eba014d0e2ef
kp-Captain02 <69666490+kp-Captain02@users.noreply.github.com>
2023-10-10 23:22:07 +0530

    commit

--- DjangoProject/buzzmap/main/models.py
+++ DjangoProject/buzzmap/main/models.py
@@ -1,29 +1,7 @@
 from django.db import models
 
-# Define the User model for managing users
-class User(models.Model):
-    username = models.CharField(max_length=100)
-    email = models.EmailField(unique=True)
-    password = models.CharField(max_length=100)
-    # Add other user-related fields as needed
+class users(models.Model):
+    service_icon=models.CharField(max_length=50)
+    service_title=models.CharField(max_length=50)
+    service_description = models.TextField(max_length=40)
 
-    def __str__(self):
-        return self.username
-
-# Define the Bus model for managing buses and drivers
-class Bus(models.Model):
-    bus_number = models.CharField(max_length=20)
-    driver_name = models.CharField(max_length=100)
-    # Add other bus-related fields as needed
-
-    def __str__(self):
-        return self.bus_number
-
-# Define the Route model for managing routes and landmarks
-class Route(models.Model):
-    route_name = models.CharField(max_length=100)
-    landmarks = models.TextField()
-    # Add other route-related fields as needed
-
-    def __str__(self):
-        return self.route_name
